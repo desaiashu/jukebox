6243761eadbc38586f46b71f2041d8637b28230d
Ashu Desai <ashuddesai@gmail.com>
2015-08-09 21:27:49 -0400

    fixed overlap issues and potentially mom's issue

--- jukebox/ConfirmationCodeViewController.swift
+++ jukebox/ConfirmationCodeViewController.swift
@@ -31,7 +31,7 @@ class ConfirmationCodeViewController: UIViewController {
         return true
     }
     
-    @IBAction func go(sender: UIButton) {
+    @IBAction func goPressed(sender: UIButton) {
         self.confirmationCodeTextField.resignFirstResponder()
         self.statusLabel.text = "Loading..."
         self.statusLabel.hidden = false
