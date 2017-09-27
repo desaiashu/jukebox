d58b49cacabed366be9c1e98f40886b95a94f98e
Ashu Desai <ashuddesai@gmail.com>
2017-09-26 20:42:59 -0700

    updating dependencies

--- Carthage/Checkouts/XCDYouTubeKit/CHANGELOG.md
+++ Carthage/Checkouts/XCDYouTubeKit/CHANGELOG.md
@@ -1,3 +1,16 @@
+#### Version 2.5.6
+
+* Xcode 9 compatibility (#345, #347, #349)
+
+#### Version 2.5.5
+
+* Adaptation to YouTube API change. (#314)
+
+#### Version 2.5.4
+
+* Adaptation to YouTube API change. (#295, #296, #297)
+* Upgrade deployment target to iOS 8.0 (macOS is still 10.9 and tvOS is still 9.0)
+
 #### Version 2.5.3
 
 * Adaptation to YouTube API change. (#257)
--- Carthage/Checkouts/XCDYouTubeKit/Scripts/release.sh
+++ Carthage/Checkouts/XCDYouTubeKit/Scripts/release.sh
@@ -4,8 +4,8 @@ function update_badges()
 {
     echo "Updating badges"
     set -v
-    sed -i "" "s/$1\.svg/$2.svg/g" "README.md"
-    sed -i "" "s/branch=$1/branch=$2/g" "README.md"
+    sed -i "" "s#$1\.svg#$2.svg#g" "README.md"
+    sed -i "" "s#branch/$1#branch/$2#g" "README.md"
     set +v
     git add "README.md"
     git commit -m "Point badges to the $2 branch"
@@ -36,11 +36,14 @@ sed -i "" "s/DYLIB_CURRENT_VERSION = .*;/DYLIB_CURRENT_VERSION = ${VERSION};/g" 
 sed -i "" "s/CURRENT_PROJECT_VERSION = .*;/CURRENT_PROJECT_VERSION = ${CURRENT_PROJECT_VERSION};/g" "XCDYouTubeKit.xcodeproj/project.pbxproj"
 sed -i "" "s/CURRENT_PROJECT_VERSION = .*;/CURRENT_PROJECT_VERSION = ${VERSION};/g" "XCDYouTubeKit Demo/XCDYouTubeKit Demo.xcodeproj/project.pbxproj"
 sed -i "" "s/^\(.*s.version.*=.*\)\".*\"/\1\"${VERSION}\"/" "XCDYouTubeKit.podspec"
+sed -i "" "s/module_version: .*/module_version: ${VERSION}/" ".jazzy.yaml"
+sed -i "" "s/tree\\/.*\\/XCDYouTubeKit/tree\\/${VERSION}\\/XCDYouTubeKit/" ".jazzy.yaml"
 sed -E -i "" "s/~> [0-9\.]+/~> ${VERSION_PARTS[0]}.${VERSION_PARTS[1]}/g" "README.md"
 set +v
 git add "XCDYouTubeKit.xcodeproj"
 git add "XCDYouTubeKit Demo/XCDYouTubeKit Demo.xcodeproj"
 git add "XCDYouTubeKit.podspec"
+git add ".jazzy.yaml"
 git add "README.md"
 git commit -m "Update version to ${VERSION}"
 
@@ -57,7 +60,7 @@ update_badges "master" "develop"
 
 echo "Things remaining to do"
 echo "  * git push with tags (master and develop)"
-echo "  * check that build is passing on travis: https://travis-ci.org/0xced/XCDYouTubeKit/"
+echo "  * check that build is passing on CircleCI: https://circleci.com/gh/0xced/XCDYouTubeKit"
 echo "  * pod trunk push XCDYouTubeKit.podspec"
 echo "  * create a new release on GitHub: https://github.com/0xced/XCDYouTubeKit/releases/new"
 echo "  * close milestone on GitHub if applicable: https://github.com/0xced/XCDYouTubeKit/milestones"
